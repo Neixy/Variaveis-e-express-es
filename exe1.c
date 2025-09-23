feccaa6fb8717b760f7816c5758a2ba4e322e6ef
Neixy <133177964+Neixy@users.noreply.github.com>
2025-09-22 22:24:50 -0300

    Exercícios do Programação Descomplicada, lista 1

---
+++ exe1.c
@@ -0,0 +1,10 @@
+/*1. Faca um programa que leia um numero inteiro e o imprima.*/
+#include <stdio.h>
+int main(){
+
+    int nro;
+    printf("Digite um nro inteiro: ");
+    scanf("%d", &nro);
+    printf("Nro digitado: %d", nro);
+
+}
